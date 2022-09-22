2fdb73088d29078577e63c0378499334376fbd6e
AdrielScheffer <adri.scheffer5@gmail.com>
2022-09-22 19:08:05 -0300

    solving errors to upload to railway

--- posts/admin.py
+++ posts/admin.py
@@ -4,6 +4,7 @@ from xml.dom.minidom import Comment
 from django.contrib import admin
 from .models import Post, Category, UserProfile,Comment
 # Register your models here.
+
 admin.site.register(Post)
 admin.site.register(Category)
 admin.site.register(UserProfile)
